bca504795fda2b9bd586c2cc2af8ec3a966b9b76
Artsiom Leuchanka <artem.levchenko.vrp@gmail.com>
2023-10-03 11:52:19 +0200

    [NBA-API-01] - Initial commit

---
+++ src/main/java/com/artbender/nba/legends/api/NbaLegendsApiApplication.java
@@ -0,0 +1,13 @@
+package com.artbender.nba.legends.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaLegendsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaLegendsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artbender/nba/legends/api/NbaLegendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artbender.nba.legends.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaLegendsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
